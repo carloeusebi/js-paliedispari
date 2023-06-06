2d578209d9f64749a47c664afb45f59a8436ce38
carloeusebi <carloeusebi@gmail.it>
2023-06-06 14:45:47 +0200

    Added is palindrome function

--- js/scripts.js
+++ js/scripts.js
@@ -10,11 +10,20 @@ const evenOrOddChoice = document.getElementById("eo-choice-input");
 const evenOrOddButton = document.getElementById("eo-button");
 const evenOrOddResult = document.getElementById("eo-result");
 
-console.log(palindromeInput);
-console.log(palindromeButton);
-console.log(palindromeResult);
+//FUNCTIONS
 
-console.log(evenOrOddInput);
-console.log(evenOrOddChoice);
-console.log(evenOrOddButton);
-console.log(evenOrOddResult);
+/**
+ * Check if a given word is palindrome or not
+ * @param {string} word the word to check
+ * @returns {boolean} whether the given string is palindrome or not
+ */
+
+function isPalindrome(word) {
+  let newWord = "";
+
+  for (let i = word.length - 1; i >= 0; i--) {
+    newWord += word.at(i);
+  }
+
+  return word === newWord;
+}
