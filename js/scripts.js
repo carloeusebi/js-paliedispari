a93337d0774efda7516bde46d3b6aab2e96dfe87
carloeusebi <carloeusebi@gmail.it>
2023-06-06 14:35:47 +0200

    Grabbed all dom elements

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,20 @@
-console.log("ok");
+// DOM elements
+// Palindrome Elements
+const palindromeInput = document.getElementById("palindrome-input");
+const palindromeButton = document.getElementById("palindrome-button");
+const palindromeResult = document.getElementById("palindrome-result");
+
+// Even or Odd Elements
+const evenOrOddInput = document.getElementById("number-input");
+const evenOrOddChoice = document.getElementById("eo-choice-input");
+const evenOrOddButton = document.getElementById("eo-button");
+const evenOrOddResult = document.getElementById("eo-result");
+
+console.log(palindromeInput);
+console.log(palindromeButton);
+console.log(palindromeResult);
+
+console.log(evenOrOddInput);
+console.log(evenOrOddChoice);
+console.log(evenOrOddButton);
+console.log(evenOrOddResult);
